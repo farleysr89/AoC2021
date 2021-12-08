50528105c6f7635a084c88939e785634a195b097
Farley <33631912+farleysr89@users.noreply.github.com>
2021-12-07 18:06:52 -0600

    Day03 progress

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -1,5 +1,4 @@
-﻿namespace AoC2021
-﻿using System;
+﻿﻿using System;
 using System.IO;
 using System.Linq;
 
@@ -9,10 +8,6 @@ namespace AoC2021
     {
         static void Main(string[] args)
         {
-            //Part1();
-            //Part2();
-        }
-    }
             Part1();
             //Part2();
         }
@@ -20,7 +15,23 @@ namespace AoC2021
         static void Part1()
         {
             string input = File.ReadAllText("input.txt");
-            var length = input[0].ToString().Length;
+            var entries = input.Split('\n',StringSplitOptions.RemoveEmptyEntries).ToList();
+            var length = entries[0].ToString().Length;
+            var gammaOutput = "";
+            var epsilonOutput = "";
+            for(var i = 0; i < length; i++)
+            {
+                int zeroCount = 0;
+                int oneCount = 0;
+                foreach(var s in entries)
+                {
+                    if(s[i] == '0') zeroCount++;
+                    else oneCount++;
+                }
+                gammaOutput += zeroCount > oneCount ? "0" : "1";
+                epsilonOutput += zeroCount > oneCount ? "1" : "0";
+            }
+            Console.WriteLine(Convert.ToInt32(gammaOutput,2) * Convert.ToInt32(epsilonOutput, 2));
         }
     }
     
