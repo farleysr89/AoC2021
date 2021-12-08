6d798240b7cc9b3798601f2a131b11fff84174d4
Farley <33631912+farleysr89@users.noreply.github.com>
2021-12-08 14:27:21 -0600

    Day04 Setup

---
+++ Day04/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace AoC2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Part1();
+            Part2();
+        }
+
+        static void Part1()
+        {
+            string input = File.ReadAllText("input.txt");
+            var entries = input.Split('\n', StringSplitOptions.RemoveEmptyEntries).ToList();
+
+        }
+
+        static void Part2()
+        {
+            string input = File.ReadAllText("input.txt");
+            var entries = input.Split('\n', StringSplitOptions.RemoveEmptyEntries).ToList();
+        }
+    }
+
+}
