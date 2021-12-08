875b8c6c794df7276ce2dc85d1829fe60d47b158
Farley <33631912+farleysr89@users.noreply.github.com>
2021-12-08 17:43:24 -0600

    Day04 Part2 Complete

--- Day04/Program.cs
+++ Day04/Program.cs
@@ -69,6 +69,60 @@
         {
             string input = File.ReadAllText("input.txt");
             var entries = input.Split('\n', StringSplitOptions.RemoveEmptyEntries).ToList();
+            var numbersToCall = entries[0].Split(',').Select(n => Convert.ToInt32(n));
+            var boards = new List<Board>();
+
+            for (var i = 1; i < entries.Count; i++)
+            {
+                var board = new Board()
+                {
+                    id = boards.Count
+                };
+                for (var j = 0; j < 6; j++)
+                {
+                    if(j == 0) continue;
+                    var row = entries[i + j].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                    var col = 0;
+                    foreach (var c in row)
+                    {
+                        board.cells.Add(new Cell()
+                        {
+                            row = j - 1,
+                            col = col,
+                            value = Convert.ToInt32(c),
+                            marked = false
+                        });
+                        col++;
+                    }
+                }
+                boards.Add(board);
+                i += 5;
+            }
+
+            foreach (var num in numbersToCall)
+            {
+                foreach (var b in boards.Where(bb => bb.rank == -1))
+                {
+                    if (b.cells.Any(c => c.value == num))
+                    {
+                        b.cells.Where(c => c.value == num).Select(c =>
+                        {
+                            c.marked = true;
+                            return c;
+                        }).ToList();
+                        if (b.checkForWin())
+                        {
+                            b.rank = boards.Count(bb => bb.rank != -1);
+                            if (boards.All(bb => bb.rank != -1))
+                            {
+                                var unMarked = b.cells.Where(c => !c.marked).Sum(c => c.value);
+                                Console.WriteLine("Solution 2 = " + unMarked * num);
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 
@@ -84,6 +138,7 @@
     {
         internal List<Cell> cells = new();
         internal int id;
+        internal int rank = -1;
 
         internal bool checkForWin()
         {
