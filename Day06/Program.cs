979c864356c215c146d59cacbd588df463f0bae5
Farley <33631912+farleysr89@users.noreply.github.com>
2021-12-09 19:07:05 -0600

    Day06 Part1 Setup

---
+++ Day06/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace AoC2021
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Part1();
+            Part2();
+        }
+
+        static void Part1()
+        {
+            var input = File.ReadAllText("testInput.txt");
+            var entries = input.Split('\n', StringSplitOptions.RemoveEmptyEntries).ToList();
+            var fish = entries[0].Split(",").Select(i => new LanternFish
+            {
+                count = Convert.ToInt32(i)
+            });
+        }
+
+        static void Part2()
+        {
+
+        }
+    }
+
+    internal class LanternFish
+    {
+        internal int count;
+    }
+}
