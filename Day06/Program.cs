6593ca4b03c864328f12a78056a69da2b318fd7a
Farley <33631912+farleysr89@users.noreply.github.com>
2021-12-10 11:41:39 -0600

    Day06 Complete

--- Day06/Program.cs
+++ Day06/Program.cs
@@ -36,28 +36,37 @@
 
         static void Part2()
         {
-            var input = File.ReadAllText("testInput.txt");
+            var input = File.ReadAllText("input.txt");
             var entries = input.Split('\n', StringSplitOptions.RemoveEmptyEntries).ToList();
-            var fish = entries[0].Split(",").Select(i => new LanternFish
+            var fish = entries[0].Split(",").Select(i => Convert.ToInt32(i)
+            ).ToList();
+            var totals = new Dictionary<int, long>();
+            foreach(var f in fish)
             {
-                count = Convert.ToInt32(i)
-            }).ToList();
+                totals.TryGetValue(f, out var currentCount);
+                totals[f] = currentCount + 1;
+            }
             for (var i = 0; i < 256; i++)
             {
-                var addCount = 0;
-                foreach (var f in fish)
+                var newTotals = new Dictionary<int, long>();
+                long addCount = 0;
+                foreach(var f in totals)
                 {
-                    f.count--;
-                    if (f.count == -1)
+                    if (f.Key == 0)
                     {
-                        f.count = 6;
-                        addCount++;
+                        newTotals.Add(6, f.Value);
+                        addCount = f.Value;
+                    }
+                    else
+                    {
+                        newTotals.TryGetValue(f.Key - 1, out var currentCount);
+                        newTotals[f.Key - 1] = currentCount + f.Value;
                     }
                 }
-                for(var j = 0; j < addCount; j++)
-                    fish.Add(new LanternFish{count = 8});
+                newTotals[8] = addCount;
+                totals = new Dictionary<int,long>(newTotals);
             }
-            Console.WriteLine("Solution 2 = " + fish.Count);
+            Console.WriteLine("Solution 2 = " + totals.Sum(t => t.Value));
         }
     }
 
