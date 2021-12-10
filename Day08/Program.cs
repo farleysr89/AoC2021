b110ce799542132425638741cbf587bb4c991a24
Farley <33631912+farleysr89@users.noreply.github.com>
2021-12-10 17:51:14 -0600

    Day08 Part 1 Complete

---
+++ Day08/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace AoC2021
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Part1();
+            Part2();
+        }
+
+        static void Part1()
+        {
+            var input = File.ReadAllText("input.txt");
+            var outputs = input.Split("\n", StringSplitOptions.RemoveEmptyEntries).ToList();
+            var parts = outputs.Select(o => o.Split(" | ")).Select(s => s[1]).SelectMany(s => s.Split(" ")).ToList();
+            var count = parts.Count(o => o.Length is 2 or 3 or 4 or 7);
+            Console.WriteLine("Solution 1 = " + count);
+        }
+
+        static void Part2()
+        {
+
+        }
+    }
+}
