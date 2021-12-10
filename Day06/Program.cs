8f47ed951987ac14174a934278955670d0e0bcc8
Farley <33631912+farleysr89@users.noreply.github.com>
2021-12-09 19:21:43 -0600

    Day06 Part 1 Complete

--- Day06/Program.cs
+++ Day06/Program.cs
@@ -15,12 +15,49 @@
             var fish = entries[0].Split(",").Select(i => new LanternFish
             {
                 count = Convert.ToInt32(i)
-            });
+            }).ToList();
+            for (var i = 0; i < 80; i++)
+            {
+                var addCount = 0;
+                foreach (var f in fish)
+                {
+                    f.count--;
+                    if (f.count == -1)
+                    {
+                        f.count = 6;
+                        addCount++;
+                    }
+                }
+                for(var j = 0; j < addCount; j++)
+                    fish.Add(new LanternFish{count = 8});
+            }
+            Console.WriteLine("Solution 1 = " + fish.Count);
         }
 
         static void Part2()
         {
-
+            var input = File.ReadAllText("testInput.txt");
+            var entries = input.Split('\n', StringSplitOptions.RemoveEmptyEntries).ToList();
+            var fish = entries[0].Split(",").Select(i => new LanternFish
+            {
+                count = Convert.ToInt32(i)
+            }).ToList();
+            for (var i = 0; i < 256; i++)
+            {
+                var addCount = 0;
+                foreach (var f in fish)
+                {
+                    f.count--;
+                    if (f.count == -1)
+                    {
+                        f.count = 6;
+                        addCount++;
+                    }
+                }
+                for(var j = 0; j < addCount; j++)
+                    fish.Add(new LanternFish{count = 8});
+            }
+            Console.WriteLine("Solution 2 = " + fish.Count);
         }
     }
 
