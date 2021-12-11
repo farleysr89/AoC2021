5c15e62ce65f67389084c95201ae88477c6f0b4e
Farley <33631912+farleysr89@users.noreply.github.com>
2021-12-11 12:12:00 -0600

    Day10 Complete

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -78,7 +78,76 @@ namespace AoC2021
 
         static void Part2()
         {
+            var input = File.ReadAllText("input.txt");
+            var lines = input.Split("\n", StringSplitOptions.RemoveEmptyEntries).ToList();
+            var scores = new List<long>();
+            foreach (var line in lines)
+            {
+                var found = false;
+                var stack = new Stack<char>();
+                foreach (var c in line.TakeWhile(c => !found))
+                {
+                    char cc;
+                    switch (c)
+                    {
+                        case('('):
+                            stack.Push(c);
+                            break;
+                        case(')'):
+                            cc = stack.Pop();
+                            if (cc != '(')
+                                found = true;
+                            break;
+                        case('['):
+                            stack.Push(c);
+                            break;
+                        case(']'):
+                            cc = stack.Pop();
+                            if (cc != '[')
+                                found = true;
+                            break;
+                        case('<'):
+                            stack.Push(c);
+                            break;
+                        case('>'):
+                            cc = stack.Pop();
+                            if (cc != '<')
+                                found = true;
+                            break;
+                        case('{'):
+                            stack.Push(c);
+                            break;
+                        case('}'):
+                            cc = stack.Pop();
+                            if (cc != '{')
+                                found = true;
+                            break;
+                        default:
+                            throw new Exception("Something Broke!");
+                    }
+                }
 
+                if (found) continue;
+                {
+                    long score = 0;
+                    while (stack.Count > 0)
+                    {
+                        var c = stack.Pop();
+                        score *= 5;
+                        score += c switch
+                        {
+                            ('(') => 1,
+                            ('[') => 2,
+                            ('{') => 3,
+                            ('<') => 4,
+                            _ => throw new Exception("Something Broke!")
+                        };
+                    }
+                    scores.Add(score);
+                }
+            }
+            scores.Sort();
+            Console.WriteLine("Solution 2 is " + scores[scores.Count / 2]);
         }
     }
 }
