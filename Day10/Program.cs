22667eaf098fcddc53c5549cf8a04edaad31087b
Farley <33631912+farleysr89@users.noreply.github.com>
2021-12-11 11:58:43 -0600

    Day10 Part 1 Complete

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace AoC2021
+﻿using System.Collections;
+
+namespace AoC2021
 {
     internal class Program
     {
@@ -10,7 +12,68 @@
 
         static void Part1()
         {
-
+            var input = File.ReadAllText("input.txt");
+            var lines = input.Split("\n", StringSplitOptions.RemoveEmptyEntries).ToList();
+            var score = 0;
+            foreach (var line in lines)
+            {
+                var found = false;
+                var stack = new Stack<char>();
+                foreach (var c in line.TakeWhile(c => !found))
+                {
+                    char cc;
+                    switch (c)
+                    {
+                        case('('):
+                            stack.Push(c);
+                            break;
+                        case(')'):
+                            cc = stack.Pop();
+                            if (cc != '(')
+                            {
+                                score += 3;
+                                found = true;
+                            }
+                            break;
+                        case('['):
+                            stack.Push(c);
+                            break;
+                        case(']'):
+                            cc = stack.Pop();
+                            if (cc != '[')
+                            {
+                                score += 57;
+                                found = true;
+                            }
+                            break;
+                        case('<'):
+                            stack.Push(c);
+                            break;
+                        case('>'):
+                            cc = stack.Pop();
+                            if (cc != '<')
+                            {
+                                score += 25137;
+                                found = true;
+                            }
+                            break;
+                        case('{'):
+                            stack.Push(c);
+                            break;
+                        case('}'):
+                            cc = stack.Pop();
+                            if (cc != '{')
+                            {
+                                score += 1197;
+                                found = true;
+                            }
+                            break;
+                        default:
+                            throw new Exception("Something Broke!");
+                    }
+                }
+            }
+            Console.WriteLine("Solution 1 is " + score);
         }
 
         static void Part2()
