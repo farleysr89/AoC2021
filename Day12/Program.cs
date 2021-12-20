497931f5bcbaf97e24b279ad5bf08d7f08d9ef60
Farley <33631912+farleysr89@users.noreply.github.com>
2021-12-20 17:37:57 -0600

    Day 12 Part 1 Complete

--- Day12/Program.cs
+++ Day12/Program.cs
@@ -10,30 +10,30 @@
 
         static void Part1()
         {
-            var input = File.ReadAllText("testInput.txt");
+            var input = File.ReadAllText("input.txt");
             var lines = input.Split("\n", StringSplitOptions.RemoveEmptyEntries).ToList();
             var paths = lines.Select(l => l.Split("-")).Select(p => new Path { ends = new End[] { new() { name = p[0] }, new() { name = p[1] } } }).ToList();
             paths.AddRange(lines.Select(l => l.Split("-")).Select(p => new Path { ends = new End[] { new() { name = p[1] }, new() { name = p[0] } } }).ToList());
             var count = 0;
             foreach (var p in paths.Where(pp => pp.ends[0].start))
             {
-                count += CountPaths(paths, p, "start", new List<string>());
+                count += CountPaths(paths, p, new List<string>{"start"});
             }
             Console.WriteLine("Solution 1 is " + count);
         }
 
-        static int CountPaths(List<Path> paths, Path path, string position, List<string> visited)
+        static int CountPaths(List<Path> paths, Path path, List<string> visited)
         {
-            var count = 0;
-            visited.Add(position);
-            var destination = path.ends[1].name;
-            visited.Add(destination);
-            if (path.ends.Any(e => e.end)) return 1;
-            foreach (var p in paths.Where(pp => pp.ends[0].name == destination && !visited.Contains(pp.ends[1].name) || pp.ends[1].large))
+            var position = path.ends[1].name;
+            if (position == "d")
             {
-                count += CountPaths(paths, p, destination, visited);
+                var debug = true;
             }
-            return count;
+            visited.Add(position);
+            //var destination = path.ends[1].name;
+            //visited.Add(destination);
+            if (path.ends[1].end) return 1;
+            return paths.Where(pp => pp.ends[0].name == position && (!visited.Contains(pp.ends[1].name) || pp.ends[1].large)).Sum(p => CountPaths(paths, p, new List<string>(visited)));
         }
 
         static void Part2()
