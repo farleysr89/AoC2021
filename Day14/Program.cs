dbca0829d5d253d158de542717ad421fe9516702
Farley <33631912+farleysr89@users.noreply.github.com>
2021-12-20 18:56:16 -0600

    Day 14 Setup

---
+++ Day14/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace AoC2021
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Part1();
+            Part2();
+        }
+
+        static void Part1()
+        {
+
+        }
+
+        static void Part2()
+        {
+
+        }
+    }
+}
