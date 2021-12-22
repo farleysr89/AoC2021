95271b0ab37f833249ff46383c0c3de7911bd515
Farley <33631912+farleysr89@users.noreply.github.com>
2021-12-22 17:43:12 -0600

    Day 15 Complete

--- Day15/Program.cs
+++ Day15/Program.cs
@@ -51,7 +51,101 @@
 
         static void Part2()
         {
+            var input = File.ReadAllText("input.txt");
 
+            var lines = input.Split("\n", StringSplitOptions.RemoveEmptyEntries).ToList();
+            var y = 0;
+            var x = 0;
+            var nodes = new Dictionary<(int, int), Node>();
+            foreach (var line in lines)
+            {
+                x = 0;
+                foreach (var c in line)
+                {
+                    nodes.Add((x,y),new Node
+                    {
+                        x = x,
+                        y = y,
+                        value = int.Parse(c.ToString()),
+                        distance = y == 0 && x == 0 ? 0 : int.MaxValue
+                    });
+                    x++;
+                }
+                y++;
+            }
+
+            var maxX = x * 5;
+            var maxY = y * 5;
+            var length = x;
+            for (var i = 0; i < maxY; i++)
+            {
+                for (var j = 0; j < maxX; j++)
+                {
+                    if (nodes.ContainsKey((j, i))) continue;
+                    var val = nodes[(j % length, i % length)].value;
+                    val += i / length + j / length;
+                    val %= 9;
+                    if (val == 0) val = 9;
+                    nodes.Add((j, i), new Node
+                    {
+                        x = j,
+                        y = i,
+                        value = val,
+                        distance = int.MaxValue
+                    });
+                }
+            }
+            var nodesToProcess = new Queue<(int, int)>();
+            nodesToProcess.Enqueue((0,0));
+            while(nodesToProcess.Any())
+            {
+                var (item1, item2) = nodesToProcess.Dequeue();
+                var value = nodes[(item1, item2)];
+
+                if (nodes.ContainsKey((item1 - 1, item2)))
+                {
+                    var node1 = nodes[(item1 - 1, item2)];
+                    var distance = Math.Min(node1.distance, node1.value + value.distance);
+                    if (distance < node1.distance)
+                    {
+                        node1.distance = Math.Min(node1.distance, node1.value + value.distance);
+                        nodesToProcess.Enqueue((node1.x, node1.y));
+                    }
+                }
+                if (nodes.ContainsKey((item1 + 1, item2)))
+                {
+                    var node1 = nodes[(item1 + 1, item2)];
+                    var distance = Math.Min(node1.distance, node1.value + value.distance);
+                    if (distance < node1.distance)
+                    {
+                        node1.distance = Math.Min(node1.distance, node1.value + value.distance);
+                        nodesToProcess.Enqueue((node1.x, node1.y));
+                    }
+                }
+                if (nodes.ContainsKey((item1, item2 - 1)))
+                {
+                    var node1 = nodes[(item1, item2 - 1)];
+                    var distance = Math.Min(node1.distance, node1.value + value.distance);
+                    if (distance < node1.distance)
+                    {
+                        node1.distance = Math.Min(node1.distance, node1.value + value.distance);
+                        nodesToProcess.Enqueue((node1.x, node1.y));
+                    }
+                }
+                if (nodes.ContainsKey((item1, item2 + 1)))
+                {
+                    var node1 = nodes[(item1, item2 + 1)];
+                    var distance = Math.Min(node1.distance, node1.value + value.distance);
+                    if (distance < node1.distance)
+                    {
+                        node1.distance = Math.Min(node1.distance, node1.value + value.distance);
+                        nodesToProcess.Enqueue((node1.x, node1.y));
+                    }
+                }
+
+            }
+
+            Console.WriteLine("Solution 2 is = " + nodes[(maxX - 1,maxY - 1)].distance);
         }
     }
 
