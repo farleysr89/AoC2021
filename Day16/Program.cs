45933b6e980bdc66254013676e455db92ef23b70
Farley <33631912+farleysr89@users.noreply.github.com>
2021-12-22 17:52:45 -0600

    Day 16 Setup

---
+++ Day16/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace AoC2021
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Part1();
+            Part2();
+        }
+
+        static void Part1()
+        {
+            var input = File.ReadAllText("testInput.txt");
+            var binary = Convert.ToString(Convert.ToInt32(input, 16), 2);
+        }
+
+        static void Part2()
+        {
+
+        }
+    }
+}
